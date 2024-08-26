8ca31020188e452386ed77f12791332e3d07b531
GauravNegi <negigaurav637@gmail.com>
2024-08-26 12:00:15 +0530

    done server

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const connectBD = require('./config/connectDB');
 connectBD();
 app.options('*', cors()); // Allow preflight requests for all routes
 app.use(cors({
-    origin: '*',
+    origin: '*', // aloow all url 
     credentials : true,
 }));
 
